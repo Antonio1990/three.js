910e74cb70116cc137a0ae99089d326130707514
Antonio1990 <350994839@qq.com>
2020-04-30 17:19:34 +0800

    测试

--- src/renderers/webgl/WebGLShader.js
+++ src/renderers/webgl/WebGLShader.js
@@ -2,23 +2,13 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-
 /**
-<<<<<<< HEAD
 属性：
  gl--webgl上下文对象
  type--gl.VERTEX_SHADER 或 gl.FRAGMENT_SHADER
  string--shader代码字符串
 返回：
  返回一个webgl着色器对象
-=======
-屬性：
- gl--webgl上下文對象
- type--gl.VERTEX_SHADER 或 gl.FRAGMENT_SHADER
- string--shader代碼字符串
-返回：
- 返回一個webgl著色器對象
->>>>>>> 0795d0aba4145bfbc19263b51d3e4e5ff4e15161
 */
 function WebGLShader( gl, type, string ) {
 
