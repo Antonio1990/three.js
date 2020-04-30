0795d0aba4145bfbc19263b51d3e4e5ff4e15161
Antonio1990 <350994839@qq.com>
2020-04-30 16:14:51 +0800

    測試

--- src/renderers/webgl/WebGLShader.js
+++ src/renderers/webgl/WebGLShader.js
@@ -2,6 +2,15 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
+
+/**
+屬性：
+ gl--webgl上下文對象
+ type--gl.VERTEX_SHADER 或 gl.FRAGMENT_SHADER
+ string--shader代碼字符串
+返回：
+ 返回一個webgl著色器對象
+*/
 function WebGLShader( gl, type, string ) {
 
 	var shader = gl.createShader( type );
